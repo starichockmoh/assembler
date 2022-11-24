f17e228020d19d0f8a9e496922efd1dcf960e8f7
Янущик Илья Андреевич <ilya.yanushchik@satellite-soft.ru>
2022-11-25 01:09:24 +0400

    task 5

--- test.asm
+++ test.asm
@@ -1,159 +1,107 @@
 .model small
 .stack 100h
 .data
+.386
 Student DB 'Sakhnov Maksim 251', 0Dh, 0Ah, '$' ; определяем переменную со строкой и переносом на следующую строку
-hi db 'hello world$'
+row db 10 ; колонка
+col db 20 ; столбец
+mode db (?) ; режим
 BreakLine DB 0Dh, 0Ah, '$'
-mas dw 10 dup(?)
+
+
 
 .code
 
-;-------------------------------------
-; Процедура Header проводит первоначальные настройки, выводит имя фамилию номер группы студента
-Header Proc
-mov ax, @data
-mov ds, ax
-xor si, si
-xor di,di
-mov ah, 09h
-mov dx, offset Student ; выводим после прерывания данные которые храняться в переменной Студент
-int 21h
-RET
-Header ENDP
-;----------------------------------
-; Процедура производит переход на следующую строку
-nextLine Proc
-mov ah, 09h
-mov dx, offset BreakLine ;
-int 21h
-RET
-nextLine ENDP
-;----------------------------------
-tabulation Proc
-xor dx, dx
-mov ah, 02h
-mov dx, '  '
-int 21h
+B10MODE PROC
+    ;получаем текущий режим 
+    mov ah, 0Fh
+    int 10h
+    mov mode, al
+
+    MOV AH,00h ;Запрос на установку видеорежима
+    MOV AL,03h ;Стандартный цветной текстовый режим
+    INT 10h    ;Вызвать обработчик прерывания
 ret
-tabulation endp
-;-----------------------------------
+B10MODE endp
 
-input Proc
-mov cx, 10
-mov si, 0
-mov ax, 1
-inputf: mov mas[si], ax
-inc ax
-add si, 2
-loop inputf
+D10CURSOR proc
+    PUSHA   ;сохраняем в стек содержимое всех регистров общего назначение
+    mov ah, 02h
+    mov bh, 1
+    mov dh, 10
+    mov dl, 20
+    int 10h
+    POPA
 ret
-input ENDP
+D10CURSOR endp
 
-;----------------------------------
-chetn Proc
-mov cx, 10
-xor si, si
-startChetn: mov ax, mas[si]
-test ax,1
-jnz finishChetn ; если нечетное переходим в конец, если четное
-;выполняем это
-push cx
-mov cx, 0
-cmp ax, 9
-jle cout1chisl
-;--------------вывод чисел больше 9
-xor dx,dx
-mov bx, 10
-delenieStart:
-div bx
-push dx
-inc cx
-cmp ax, 0
-jne delenieStart
 
-xor ax,ax
-coutStart: pop dx
-add dx,30h
-mov ah, 02h
-int 21h
-loop coutStart
-jmp ifBigger10
-;--------------вывод чисел больше 9 кончился
+C10CLEAR proc
+    pusha
 
-cout1chisl:  ;вывод числа от 1 до 9
-mov dx, ax
-add dx, 30h
-mov ah, 02h
-int 21h
+    MOV AH,06h   ;Запросить прокрутку вверх
+    MOV AL,01h   ;на одну строку
+    MOV CX,0000  ;от 00:00 до
+    MOV DX,18FFh ;24:79 (весь экран)
+    INT 10h      ;Вызвать обработчик прерывания
 
-ifBigger10: call tabulation
-pop cx
-finishChetn: add si,2
-loop startChetn
+    popa
 ret
-chetn ENDP
+C10CLEAR endp
 
-;----------------------------------
+B10DISPLAY proc 
+pusha
+    mov al, 44h
+    mov ah, 05h
+    mov di, 1640
+    mov cx, 10
+    i:
+        push cx
+        mov cx, 10
 
+        j: 
+            mov es:word ptr[di], ax
+            add di, 2
+        loop j
 
-nechetn PROC
-mov cx, 10
-xor si, si
-startneChetn: mov ax, mas[si]
-test ax,1
-jz finishneChetn ; если нечетное переходим в конец, если четное
-;выполняем это
-push cx
-mov cx, 0
-cmp ax, 9
-jle cout1chisln
-;--------------вывод чисел больше 9
-xor dx,dx
-mov bx, 10
-delenieStartn:
-div bx
-push dx
-inc cx
-cmp ax, 0
-jne delenieStartn
+        add di, 140
+        pop cx
+        add ah,1
+        add al, 1
+    loop i
+    
 
-xor ax,ax
-coutStartn: pop dx
-add dx,30h
-mov ah, 02h
-int 21h
-loop coutStartn
-jmp ifBigger10n
-;--------------вывод чисел больше 9 кончился
-
-cout1chisln:
-mov dx, ax
-add dx, 30h
-mov ah, 02h
-int 21h
-
-ifBigger10n: call tabulation
-pop cx
-finishneChetn: add si,2
-loop startneChetn
 ret
-nechetn ENDP
+B10DISPLAY endp
 
-;----------------------------------
-
-solve Proc
-call input
-call chetn
-call nextline
-call nechetn
-ret
-solve ENDP
 
 
 start:
-call Header
+    mov ax,@data
+    mov ds,ax
+    ;Код главной программы
+ 
+    mov ax,0b900h         ;Используя сегментный регистр ES,   
+    mov es,ax             ;организовать запись данных в видеопамять
+                          ;по адресу В900h:0000h (страница 1)
+    call B10MODE
+    call C10CLEAR
+    
 
-call solve
+    MOV AH,05h ;Выбор функции для вывода страницы
+    MOV AL,01h ;Страница 1
+    INT 10h    ;Вызвать обработчик прерывания
+    call D10CURSOR
+
+    call B10DISPLAY
+
+    xor ax,ax
+    mov ah, 10h
+    int 16h
+    
+    MOV AH,00h ;Запрос на установку видеорежима
+    MOV AL, mode 
+    INT 10h    ;Вызвать обработчик прерывания
 
 mov AX,4C00h
 int 21h
